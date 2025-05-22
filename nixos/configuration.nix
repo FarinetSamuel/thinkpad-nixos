531b96850d90934bd41bbf97e118b10463383b7d
FarinetSamuel <ros.richard@gmail.com>
2025-05-22 06:37:01 +0200

    Delete nixos directory

--- nixos/configuration.nix
+++
@@ -1,8 +0,0 @@
-# Ce fichier est maintenu pour la rétrocompatibilité avec des commandes comme
-# `sudo nixos-rebuild switch` (sans --flake)
-
-{ config, pkgs, ... }:
-
-{
-  imports = [ ./hosts/thinkpad.nix ];
-}
